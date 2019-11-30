7714c289e24825bfe940cffeb14d4adac09cd97e
Shigma <1700011071@pku.edu.cn>
2019-11-30 17:22:57 +0800

    init

---
+++ src/group.ts
@@ -0,0 +1,52 @@
+import { joinKeys, formatValues } from './database'
+import { isSubset } from 'koishi-utils'
+import { assignees as assigneeIds, injectMethods, GroupData, createGroup, groupFields } from 'koishi-core'
+
+type CachedGroupData = GroupData & { _timestamp: number }
+
+const GROUP_REFRESH_INTERVAL = 60 * 1000
+const groupCache: Record<number, CachedGroupData> = {}
+
+injectMethods('mysql', {
+  async getGroup (groupId, selfId = 0, fields = groupFields) {
+    const timestamp = Date.now()
+    const cache = groupCache[groupId]
+    if (cache && isSubset(fields, Object.keys(cache)) && timestamp - cache._timestamp < GROUP_REFRESH_INTERVAL) {
+      return cache
+    }
+
+    const [data] = await this.query('SELECT ' + joinKeys(fields) + ' FROM `groups` WHERE `id` = ?', [groupId]) as GroupData[]
+    let fallback: GroupData
+    if (!data) {
+      fallback = createGroup(groupId, selfId)
+      if (selfId && groupId) {
+        await this.query(
+          'INSERT INTO `groups` (' + joinKeys(groupFields) + ') VALUES (' + groupFields.map(() => '?').join(', ') + ')',
+          formatValues('groups', fallback, groupFields),
+        )
+      }
+    } else {
+      data.id = groupId
+    }
+
+    const group = groupCache[groupId] = (data || fallback) as CachedGroupData
+    Object.defineProperty(group, '_timestamp', { value: timestamp })
+    return group
+  },
+
+  async getAllGroups (keys = groupFields, assignees = assigneeIds) {
+    let queryString = 'SELECT ' + joinKeys(keys) + ' FROM `groups`'
+    if (assignees) queryString += ` WHERE \`assignee\` IN (${assignees.join(',')})`
+    return this.query(queryString)
+  },
+
+  async setGroup (groupId, data) {
+    const result = await this.update('groups', groupId, data)
+    if (!groupCache[groupId]) {
+      groupCache[groupId] = {} as CachedGroupData
+      Object.defineProperty(groupCache[groupId], '_timestamp', { value: Date.now() })
+    }
+    Object.assign(groupCache[groupId], data)
+    return result
+  },
+})
---
+++ src/user.ts
@@ -0,0 +1,56 @@
+import { MysqlDatabase, joinKeys, formatValues, arrayTypes } from './database'
+import { observe, complement } from 'koishi-utils'
+import { injectMethods, userFields, UserData, createUser, User } from 'koishi-core'
+
+arrayTypes.push('users.endings', 'users.achievement', 'users.inference')
+
+injectMethods('mysql', {
+  async getUser (userId, defaultAuthority = 0, fields = userFields) {
+    const [data] = await this.query('SELECT ' + joinKeys(fields) + ' FROM `users` WHERE `id` = ?', [userId]) as UserData[]
+    let fallback: UserData
+    if (data) {
+      data.id = userId
+    } else if (defaultAuthority < 0) {
+      return null
+    } else {
+      fallback = createUser(userId, defaultAuthority)
+      if (defaultAuthority) {
+        await this.query(
+          'INSERT INTO `users` (' + joinKeys(userFields) + ') VALUES (' + userFields.map(() => '?').join(', ') + ')',
+          formatValues('users', fallback, userFields),
+        )
+      }
+    }
+    return data || fallback
+  },
+
+  async getUsers (ids, fields = userFields) {
+    if (!ids.length) return []
+    return this.query(`SELECT ${joinKeys(fields)} FROM users WHERE id IN (${ids.join(', ')})`)
+  },
+
+  async getAllUsers (this: MysqlDatabase, fields = userFields) {
+    return this.query('SELECT ' + joinKeys(fields) + ' FROM `users`')
+  },
+
+  async setUser (userId, data) {
+    return this.update('users', userId, data)
+  },
+
+  async observeUser (user, defaultAuthority = 0, fields = userFields) {
+    if (typeof user === 'number') {
+      const data = await this.getUser(user, defaultAuthority, fields)
+      return data && observe(data, diff => this.setUser(user, diff), `user ${user}`)
+    } else {
+      const additionalFields = complement(fields, Object.keys(user))
+      const additionalData = additionalFields.length
+        ? await this.getUser(user.id, defaultAuthority, complement(fields, Object.keys(user)))
+        : {} as Partial<UserData>
+      if ('_diff' in user) {
+        return (user as User)._merge(additionalData)
+      } else {
+        return observe(Object.assign(user, additionalData), diff => this.setUser(user.id, diff), `user ${user.id}`)
+      }
+    }
+  },
+})
